ad00d2d821e50c47affc50049ad0403c641e0df6
Tony <tatkinson.11@gmail.com>
2016-10-17 19:21:47 -0600

    finished time_calc() & moved parsing logic into class

--- python/LogAnalysis.py
+++ python/LogAnalysis.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-import re, argparse, sys, datetime #re for regex, argparse for options, sys for argv
+import re, argparse, sys #re for regex, argparse for options, sys for argv
 #import operator, fileinput
 from time import mktime, strftime, strptime, gmtime
 
-####################################
-#-- Argument/other configuration --#
-####################################
+#### Argument/other configuration
 parser = argparse.ArgumentParser(description='Log parser - For internal testing purposes only (for now)')
 
 parser.add_argument("log_file",
@@ -40,38 +38,34 @@ args = parser.parse_args()
 
 
 
-expr_comp = re.compile(r"(.*data.*)", re.M)
-expr_pam = re.compile(r"(.*red.*)", re.M)
-log = args.log_file 
-
-
 ## REGEX PATTERNS FOUND IN 'pat_file'. STORED IN THE CURRENT WORKING DIRECTORY ##
+## Add patterns that match *from string beginning* any lines to be returned    ##
+## Commented patterns are ignored. Comments acceptable at EOL                  ##
 #eg.
-# $ cat pat_file
-# .*data.* # datatest
-# .*red.* # redtest
+'''
+$ cat pat_file
+.*data.* # datatest
+.*red.* # redtest
+# .*testing.* # testing -disabled
+'''
 
-
-####################################
-#----- Primary logic / parser -----#
-####################################
+log = args.log_file
+ 
 def main(argv, input_file):
     pat_store = ()
     ## -o ##
     if args.out is not None:
         sys.stdout = open(args.out, "w")
-        
-    ########
     
     if args.log_file: #primary - "overview output"
         try:
             with open(input_file, 'r') as parse_target, open('pat_file', 'r') as pat_list: 
                 for line in pat_list:
-		    l = line[:line.index("#")].strip()
-		    if not l.startswith("#"):
+                    l = line[:line.index("#")].strip()
+                    if not l.startswith("#"):
                         pat_store += (l,)
-		print("Matching against patterns in ./pat_file: %s" % (pat_store,))
-		patterns = re.compile('|'.join(['(%s)' % i for i in pat_store]))
+                print("Matching against patterns in ./pat_file: %s\n" % (pat_store,))
+                patterns = re.compile('|'.join(['(%s)' % i for i in pat_store]))
                 parse(parse_target, patterns)
         except IOError as err:
             print("Operation failed: %s" % (err.strerror))
@@ -79,67 +73,61 @@ def main(argv, input_file):
 
 
 
-## Core parsing logic ##
 def parse(parse_target, patterns): 
-	nss_count, pam_count = (0, 0)
-	times = []
-	i = 0
-    #for p in part(parse_target):  # parse by chunk - temporarily disabled in favor of line-by-line for now
-	for line_count, line in enumerate(parse_target, 1):
-		i += 1
-		if i == 0: time_calc(i, line, times) #run on first line only
+    nss_count, pam_count = (0, 0)
+    times = []
+    i = 0
+    reg = Reg()
+    time_chk = re.compile(r'^([A-Za-z]{3} [0-9]{2} [0-9]{2}[:]?[0-9:]+.*)$')
+    for line_count, line in enumerate(parse_target, 1):
+        timestamp = line.strip()[0:15]
+        if i == 0 and reg.match(time_chk, line): # first log entry, pull timestamp
+            time_calc(i, timestamp, times)
+            i += 1
+            
+        ## Primary matching from patterns contained in patterns tuple ##
+        if reg.match(patterns, line):
+            ## Fringe-case pattern matching - Currently demo for NSS/PAM count logic ##
+            if 'data' in reg.last_match.group(0):
+                nss_count += 1
+            if 'red'  in reg.last_match.group(0):
+                pam_count += 1
+            #print(line_count, reg.last_match.group(0))
 
-		## Primary matching from patterns contained in patterns tuple ##
-		if patterns.match(line):
-		#print(line_count, line)
-			pass
-		## Fringe-case pattern matching - Currently demo for NSS/PAM count logic ##
-		if expr_comp.match(line): 
-			nss_count += 1
-		#            print(line_count, line)
-		if expr_pam.match(line):
-			pam_count += 1
-		#            print(line_count, line)
-
-		#        for regex in patterns: 
-		#            finditer = (re.findall(regex, line, re.M))
-		## Iterate lines matching patterns provided in expr_strings[] ##
-		#            for match in finditer: 
-		#                call_count(match, line_count) 
-
-	time_calc(i, line, times)
-	print("NSS calls: '%s' in the file: %s" % (nss_count, log))
-	print("PAM calls: '%s' in the file: %s" % (pam_count, log))
+    time_calc(i, timestamp, times) # last log entry, pull timestamp
+    print("NSS calls: '%s' in the file: %s" % (nss_count, log))
+    print("PAM calls: '%s' in the file: %s" % (pam_count, log))
     
 
         
-####################################
-#----- Supplemental functions -----#
-####################################
+#### Supplemental
+
+class Reg(object):
+    def __init__(self):
+        self.last_match = None
+    def match(self,pattern,line):
+        self.last_match = pattern.match(line)
+        return self.last_match
+    def search(self,pattern,line):
+        self.last_match = pattern.search(line)
+        return self.last_match
+
+def time_calc(i, timestamp, times):
+    if i == 0: 
+        times += (mktime(strptime(timestamp, "%b %d %H:%M:%S")), )
+        print("Log start: %s" % (timestamp))
+    else:
+        times += (mktime(strptime(timestamp, "%b %d %H:%M:%S")), )
+        seconds = int(times[1]-times[0])
+        minutes = int(seconds / 60)
+        hours = int(minutes / 60)
+#       elapsed = str(strftime("%d %H:%M:%S", gmtime(seconds))) #%d has min of 01 - <1day = wrong return
+        print("Log end:   %s" % (timestamp))
+        print("Elapsed time:\n  In hours:   %s\n  In minutes: %s\n  In seconds: %s \n" % (hours, minutes, seconds))
 
 
 
-def time_calc(i, line, times):
-	time_chk = re.compile(r'^([A-Za-z]{3} [0-9]{2} [0-9]{2}[:]?[0-9:]+)$')
-	timestamp = line.strip()[0:15]
-	print(i)
-	print(timestamp)
-	if i >=1: 
-		times += (mktime(strptime(timestamp, "%b %d %H:%M:%S")), )
-		print("Starting time: %s" % (timestamp))
-	if i >= 2 and time_chk.match(timestamp):
-		times += (mktime(strptime(timestamp, "%b %d %H:%M:%S")), )
-		seconds = int(times[1]-times[0])
-		minutes = int(seconds / 60)
-		hours = int(minutes / 60)
-#		elapsed = str(strftime("%d %H:%M:%S", gmtime(seconds))) #in dev - %d has min of 01 so calc is off
-		print("Ending time:   %s" % (timestamp))
-		print("Elapsed time:\n  In hours: %s\n  In minutes: %s\n  In seconds: %s " % (hours, minutes, seconds))
-
-
-
-
-######################### testing / in development
+######################### testing / not implemented
 
 def part(fileinput, chunk=512):
     while True:
