71f7c956ef357847d56b56370ab69ed1f056513f
Tony <tatkinson.11@gmail.com>
2016-10-26 22:45:55 -0600

    performance considerations + c

--- python/testing.py
+++ python/testing.py
@@ -1,41 +1,67 @@
 #!/usr/bin/env python
 
 import re
-from collections import defaultdict
-
-FILE = "adinfo_support.txt"
-for line in pat_list:
-    if "#" in line: l = line[:line.index("#")].strip()
-    else:           l = line.strip()
-    if not l.startswith("#") and len(l) != 0: vs.pat_store += (l,)
-patterns  = re.compile( '|'.join( ['(%s)' % i for i in vs.pat_store] ) )
-
+from collections import defaultdict, namedtuple
 import time, sys
-if sys.platform == "win32":
-    timer = time.clock
-else:
-    timer = time.time
 
-t0, t1 = timer(), time.clock()
+i = 0
+def check(x):
+    global i
+    i = i + 1
 
-pat = re.compile(r".*data.* ")
+pat_store = ()
 
-search = pat.search
+with open('adinfo_support.txt', 'r') as f, open('pat_file', 'r') as pat_list:
+    for line in pat_list:
+        if "#" in line: l = line[:line.index("#")].strip()
+        else:           l = line.strip()
+        if not l.startswith("#") and len(l) != 0: pat_store += (l,)
+    patterns  = re.compile( '|'.join( ['(%s)' % i for i in pat_store] ) )
 
-# map
-matches = (search(line) for line in open(FILE, "r") if "data" in line)
-mapp    = (match.group(0) for match in matches if match)
+    
+## ~1.8sec
+#    for line in f:
+#        fields = line.split('\t')
+#        #mapp = list(map(check,fields))
+#        mapp = map(lambda z: z * 2, fields)
+#        for r in mapp:
+#            if re.match(patterns, r):
+#                pass 
 
-# reduce
-count = defaultdict(int)
-for page in mapp:
-    count[page] +=1
 
-for key in sorted(count, key=count.get)[:10]:
-    pass # print "%40s = %s" % (key, count[key])
 
-print timer() - t0, time.clock() - t1
+# ~1.5sec
+    matches = []
+    for line in f:
+        if "adclient" in line:
+            line = line.split("adclient")
+            #fields = [line[i] for i in range(5, 20)] 
+            fields = line[1]
+            #print(fields)
+            if re.match(patterns, fields):
+                m = line
+                matches.append( m )
+                pass
 
-# sanity check
-for key in sorted(count, key=count.get)[-10:]:
-    print "%40s = %s" % (key, count[key])
+
+
+
+## 2+sec
+#    search = patterns.search
+#
+#    # map
+#    matches = (search(line) for line in f if "data" in line or "red" in line)
+#    mapp    = (match.group(0) for match in matches if match)
+#
+#    # reduce
+#    count = defaultdict(int)
+#    for page in mapp:
+#        count[page] +=1
+#
+#    #for key in sorted(count, key=count.get)[:10]:
+#    #    pass # print "%40s = %s" % (key, count[key])
+#
+#
+#    # sanity check
+#    for key in sorted(count, key=count.get)[-10:]:
+#        print ("%40s = %s" % (key, count[key]))
